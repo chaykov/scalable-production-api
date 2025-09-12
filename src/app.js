e4558cf8546bcf3c0aeadc36511366f096fa6c6d
Chaykov <toumarenshi@gmail.com>
2025-09-12 17:26:01 +0200

    implement eslint and prettier

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send("Hello from ScalableAPI")
-})
+    res.status(200).send('Hello from ScalableAPI');
+});
 
 export default app;
